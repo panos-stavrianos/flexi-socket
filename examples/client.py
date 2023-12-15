a1662874d8dd59aa26c5dc3f92463bee3237c1a8
Panos Stavrianos <pstavrianosmail@gmail.com>
2023-12-15 14:58:13 +0200

    some async fixes, added on_disconnect

--- examples/client.py
+++ examples/client.py
@@ -1,24 +1,21 @@
-from time import sleep
-
 from flexi_socket import Protocol, Mode, FlexiSocket, Connection
 
-while True:
-    client = FlexiSocket(mode=Mode.CLIENT,
-                         protocol=Protocol.TCP,
-                         host="0.0.0.0",
-                         port=8080)
+client = FlexiSocket(mode=Mode.CLIENT,
+                     protocol=Protocol.TCP,
+                     host="0.0.0.0",
+                     port=8080)
 
 
-    @client.on_connect()
-    async def on_connect_(connection: Connection):
-        print(f"Connected to {connection}")
-        await connection.send("!!Hello from client 1")
-
-    @client.on_disconnect()
-    async def on_disconnect_(connection: Connection):
-        print(f"Disconnected from {connection}")
+@client.on_connect()
+async def on_connect(connection: Connection):
+    print(f"Connected to {connection}")
+    await connection.send("!!Hello from client 1")
 
 
-    client.start()
-    client.stop()
-    sleep(10)
+@client.on_disconnect()
+async def on_disconnect(connection: Connection):
+    print(f"Disconnected from {connection}")
+
+
+client.start()
+
